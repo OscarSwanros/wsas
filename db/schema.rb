a4a75b07fa977c858dc7d36c70f4961e1c003737
Oscar Swanros <swanros@gmail.com>
2014-04-06 16:35:27 -0500

    new schema for the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404044213) do
+ActiveRecord::Schema.define(version: 20140406212932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140404044213) do
     t.string   "rfc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "delayed_jobs", force: true do |t|
