f59b791be3e72a2aba1b0cc9739d6f91da520bf9
Oscar Swanros <swanros@gmail.com>
2014-04-07 15:08:29 -0500

    Schema update for the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406212932) do
+ActiveRecord::Schema.define(version: 20140407160218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,15 +55,24 @@ ActiveRecord::Schema.define(version: 20140406212932) do
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
+  create_table "expenses", force: true do |t|
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description", null: false
+    t.float    "cost",        null: false
+    t.integer  "job_id"
+    t.integer  "worker_id"
+  end
+
   create_table "jobs", force: true do |t|
     t.integer  "car_id"
-    t.integer  "worker_id"
     t.datetime "finished_at"
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "state"
     t.string   "report_number"
+    t.integer  "worker_id"
   end
 
   create_table "users", force: true do |t|
