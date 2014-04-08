7acd2166845df0f3633c9bf8c83e6c9c9818bf2a
Oscar Swanros <swanros@gmail.com>
2014-04-07 19:44:29 -0500

    Expose nifty things

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
-  expose(:clients) { Client.order(created_at: :desc).limit(5) }
-  expose(:jobs) { Job.order(updated_at: :desc).limit(5) }
+  limit = 5
+
+  expose(:clients) { Client.order(created_at: :desc).limit(limit) }
+  expose(:jobs) { Job.order(updated_at: :desc).limit(limit) }
+  expose(:quotes) { Job.where("updated_at >= ?", Time.now - 24.hours) }
+  expose(:workers) { Worker.order(updated_at: :desc).limit(limit) }
+  expose(:expenses) { Expense.where("updated_at >= ?", Time.now - 24.hours) }
+
+  def index
+    @total_investment = expenses.map(&:cost).sum
+    @total_quotes = quotes.map(&:quote).sum
+  end
 end
